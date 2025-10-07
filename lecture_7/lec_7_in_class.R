36611b0f7fbc1f5265010270aef1c78baf15254c
auffhammer <auffhammer@berkeley.edu>
2025-10-07 11:59:14 -0700

    week_7

---
+++ lecture_7/lec_7_in_class.R
@@ -0,0 +1,26 @@
+# In Class Week 7
+rm(list = ls()) # clear memory
+# Put your own working Directory Here# if you are on jupyter, use this one: 
+#Jupyter#  setwd("~/COMPSS_201_2025/lecture_6")
+setwd("/Users/auffhammer/Library/CloudStorage/Dropbox/06_Teaching/MACSS/2024/code/public-repository-1/week_7")
+options(htmltools.dir.version = FALSE)
+library(pacman)
+p_load(ggplot2,dplyr)
+#1. Read in data "class_7.csv"
+ 
+#2. Regress births on income. 
+ 
+#3. Plot your residuals against income
+ 
+#4. Plot your residuals against time
+ 
+#5. Test for AR(1) errors by using the residuals from your regression (regress residual on its lag)
+ 
+#6. Follow the steps in the slides on calculating the FGLS estimator. 
+ 
+  
+# - Estimate the original (untransformed) model; save residuals.
+# - Estimate ρ Regress residuals on their lags (no intercept).
+# - Estimate the transformed model (look at formula on slide 29 )
+
+
